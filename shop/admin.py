dffa40ceda4bcd29e1ab7bdfee5d44c140e0e92d
FREDYK1 <fkwekukankam@gmail.com>
2024-12-09 23:57:48 +0000

    Refactor admin page

--- shop/admin.py
+++ shop/admin.py
@@ -1,7 +1,28 @@
 from django.contrib import admin
 from .models import Category, Product, Order, OrderItem
 
-admin.site.register(Category)
-admin.site.register(Product)
-admin.site.register(Order)
-admin.site.register(OrderItem)
+
+class AdminCategory(admin.ModelAdmin):
+    list_display = ['name', "created_at"]
+    prepopulated_fields = {'slug': ('name',)}
+
+class AdminProduct(admin.ModelAdmin):
+    list_display = ['name', 'price', 'stock', 'category', 'created_at', 'updated_at']
+    list_editable = ['price', 'stock']
+    prepopulated_fields = {'slug': ('name',)}
+
+
+class AdminOrder(admin.ModelAdmin):
+    list_display = ['customer_name', 'customer_email', 'created_at']
+    list_filter = ['created_at']
+    search_fields = ['customer_name', 'customer_email']
+
+class AdminOrderItem(admin.ModelAdmin):
+    list_display = ['order', 'product', 'quantity']
+    list_filter = ['order', 'product']
+    search_fields = ['order', 'product']
+
+admin.site.register(Category, AdminCategory)
+admin.site.register(Product, AdminProduct)
+admin.site.register(Order, AdminOrder)
+admin.site.register(OrderItem, AdminOrderItem)
--- shop/models.py
+++ shop/models.py
@@ -4,6 +4,7 @@ from django.db import models
 class Category(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField(blank=True)
+    slug = models.SlugField(unique=True)
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
@@ -15,7 +16,9 @@ class Product(models.Model):
     price = models.DecimalField(max_digits=10, decimal_places=2)
     stock = models.IntegerField()
     category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='products')
+    slug = models.SlugField(unique=True)
     created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.name
